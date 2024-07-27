283f1d8195e8300725c587f91c7a5cc631e7f007
Sam Busto <148919619+SBUSTO@users.noreply.github.com>
2024-07-26 20:37:52 -0400

    doing some tidying! deleted uneccessary files. added extra HTML files. added my JS code from my pair program wtih Sonny. added some information to my README file. modified my original code a bit

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,100 @@
-document.addEventListener('DOMContentLoaded'), () => {
-    const agentDisplay = document.getElementById('agentDisplay');
-    const agentImage = document.getElementById('agentImage');
-    const agentButton = document.getElementById('agentButton');
-}
-
+// list of agents
 const agents = [
-    { name: 'Astra', img: 'images/Astra_icon.webp' },
-    { name: 'Breach', img: 'images/Breach_icon.webp' },
-    { name: 'Brimstone', img: 'images/Brimstone_icon.webp' },
-    { name: 'Chamber', img: 'images/Chamber_icon.webp' },
-    { name: 'Clove', img: 'images/Clove_icon.webp'},
-    { name: 'Cypher', img: 'images/Cypher_icon.webp' },
-    { name: 'Jett', img: 'images/Jett_icon.webp' },
-    { name: 'KAY/O', img: 'images/KAYO_icon.webp' },
-    { name: 'Killjoy', img: 'images/Killjoy_icon.webp' },
-    { name: 'Neon', img: 'images/Neon_icon.webp' },
-    { name: 'Omen', img: 'images/Omen_icon.webp' },
-    { name: 'Phoenix', img: 'images/Phoenix_icon.webp' },
-    { name: 'Raze', img: 'images/Raze_icon.webp' },
-    { name: 'Reyna', img: 'images/Reyna_icon.webp' },
-    { name: 'Sage', img: 'images/Sage_icon.webp' },
-    { name: 'Skye', img: 'images/Skye_icon.webp' },
-    { name: 'Sova', img: 'images/Sova_icon.webp' },
-    { name: 'Viper', img: 'images/Viper_icon.webp' },
-    { name: 'Yoru', img: 'images/Yoru_icon.webp' }
+    { name: "Astra", img: "https://placehold.co/100" },
+    { name: "Breach", img: "https://placehold.co/100" },
+    { name: "Brimstone", img: "https://placehold.co/100" },
+    { name: "Chamber", img: "https://placehold.co/100" },
+    { name: "Clove", img: "https://placehold.co/100" },
+    { name: "Cypher", img: "https://placehold.co/100" },
+    { name: "Jett", img: "https://placehold.co/100" },
+    { name: "KAY/O", img: "https://placehold.co/100" },
+    { name: "Killjoy", img: "https://placehold.co/100" },
+    { name: "Neon", img: "https://placehold.co/100" },
+    { name: "Omen", img: "https://placehold.co/100" },
+    { name: "Phoenix", img: "https://placehold.co/100" },
+    { name: "Raze", img: "https://placehold.co/100" },
+    { name: "Reyna", img: "https://placehold.co/100" },
+    { name: "Sage", img: "https://placehold.co/100" },
+    { name: "Skye", img: "https://placehold.co/100" },
+    { name: "Sova", img: "https://placehold.co/100" },
+    { name: "Viper", img: "https://placehold.co/100" },
+    { name: "Yoru", img: "https://placehold.co/100" },
 ];
 
-agentButton.addEventListener('click', () => {
-    const randomAgent = getRandomAgent();
-    agentDisplay.textContent = randomAgent.name;
-    agentImage.src = randomAgent.img;
-    agentImage.alt = '${randomAgent.name} Image';
-    agentImage.style.display = 'block';
+// get random agent button 
+const selected = [];
+
+const root = document.querySelector('#top');
+
+const randomButton = document.createElement('button');
+randomButton.innerText = 'Get Random Agent';
+randomButton.addEventListener('click', () => {
+    let random;
+    let agent;
+
+    if (selected.length) {
+        random = Math.floor(Math.random() * selected.length);
+        agents = selected[random]
+    } else {
+        random = Math.floor(Math.random() * agents.length);
+        agent = agents[random]
+    };
+
+    // displaying random agent 
+    let agentDisplayDiv = document.getElementById('agent-display');
+
+    if (!agentDisplayDiv) {
+        // if it doesnt exist, create and append it to DOM
+        agentDisplayDiv = document.createElement('div');
+        agentDisplayDiv.setAttribute('id', 'agent-display');
+
+        const agentDisplay = document.createElement('img');
+        agentDisplay.setAttribute('id', 'agent-img');
+
+        const agentName = document.createElement('p');
+        agentName.setAttribute('id', 'agent-name');
+
+        agentDisplayDiv.appendChild(agentDisplay);
+        agentDisplayDiv.appendChild(agentName);
+
+        const selectedAgent = document.getElementById("selected-agent");
+        selectedAgent.after(agentDisplayDiv);
+
+    }
+
+    // update contents of existing element
+
+    const agentImg = document.getElementById('agent-img');
+    agentImg.setAttribute('src', "selectedAgent"); // replace selectedAgent with correct image src
+    
+    const agentName = document.getElementById('agent-name');
+    agentName.innerText = agent.name;
+
 });
 
-function getRandomAgent() {
-    const randomIndex = Math.floor(Math.random() * agents.length);
-    return agents[randomIndex];
-}
+root.appendChild(randomButton);
+
+// displaying all agents
+for (let index = 0; index < agents.length; index++ ) {
+    const agent = agents[index];
+    // create a wrapper
+    const wrapper = document.createElement("button");
+    // create a unique id for the wrapper
+    const id = `agent-${index}`;
+    // create a img with src attribute
+    const agentImage = document.createElement("img");
+    agentImage.setAttribute('src', agent.img);
+    // create a p tag with the agent name inside of it
+    const agentName = document.createElement("p");
+    agentName.innerText = agent.name;
+    // add the img and p tag as parents of the wrapper
+    wrapper.appendChild(agentImage);
+    wrapper.appendChild(agentName);
+    // add event listener to handle logic we want to execute on click
+    wrapper.addEventListener('click', () => {
+        selected.push(agent);
+    })
+    // add the wrapper to the page
+    const agentWrapper = document.querySelector('#agents');
+    agentWrapper.appendChild(wrapper);
+};
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # valorant-agent-select
+
+
+# i am a junior dev, i tried to recreate https://www.valorantpicker.com/#/ to the best of my ability. i still am trying to wrap my mind around JavaScript, so bare with me
+# i would like to thank all my mentors for their help, no matter how much i never understood JavaScript
+# special thanks to Sonny for all the extra time he put in to help me
+
+# to use this website, you can simply begin by clicking the "Get Random Agent" button, this will give you a random agent (character) from all
+# if you want to get a random agent from an entire role (category), you can click "Select All" or "Unselect All" and then click "Get Random Agent" to get a random agent only from a select group
+# additionally, you can click on agents individually to create your own pool of agents to get a random agent from! 
+
+# to complete the rubric, i am going to use the following requirements in my project
+
+#
